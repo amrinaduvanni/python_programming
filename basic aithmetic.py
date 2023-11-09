8edb0aef1e7feb3e70cfeebb7dc72d12cb09bb21
amrinaduvanni <145668094+amrinaduvanni@users.noreply.github.com>
2023-11-09 15:52:43 +0530

    Add files via upload

---
+++ basic aithmetic.py
@@ -0,0 +1,11 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+addition=(num1+num2)
+print("addition",addition)
+difference=num1-num2
+print("difference",difference)
+multiplication=num1*num2
+print("multiplication",multiplication)
+division=num1/num2
+print("division",division)
+
