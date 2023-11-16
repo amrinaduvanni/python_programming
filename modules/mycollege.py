2af27311ca29d6431397d5bf333d79fc5a57923c
amrinaduvanni <145668094+amrinaduvanni@users.noreply.github.com>
2023-11-16 15:19:17 +0530

    Add files via upload

---
+++ modules/mycollege.py
@@ -0,0 +1,9 @@
+from college import staffclg as stf
+from college import studentclg as std
+from college.department import staffmca as stfmca
+from college.department import studentmca as stdmca
+
+stf.staff()
+std.student()
+stdmca.studentmca()
+stfmca.staffmca()
